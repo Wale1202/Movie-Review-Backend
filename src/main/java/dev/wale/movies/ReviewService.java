24de47fb75a288908f20883865b69b983313b0c1
Wale1202 <83563997+Wale1202@users.noreply.github.com>
2026-04-10 08:35:08 +0100

    adding the delete service

--- src/main/java/dev/wale/movies/ReviewService.java
+++ src/main/java/dev/wale/movies/ReviewService.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
-
+import org.bson,types.ObjectId;
 import java.util.List;
 
 import static org.springframework.data.mongodb.core.aggregation.SelectionOperators.First.first;
@@ -24,13 +24,23 @@ public class ReviewService {
         return reviewRepository.findAll();
     }
     public Review createReview(String reviewBody, String imdbId, String userId){
-        Review review = reviewRepository.insert(new Review(reviewBody));
+        Review review = new Review(reviewBody);
+        review.setUserId(userId);
+        review = reviewRepository.insert(review);
 
         mongoTemplate.update(Movie.class)
                 .matching(Criteria.where("imdbId").is(imdbId))
                 .apply(new Update().push("reviewIds").value(review))
                 .first();
-
+    
         return review;
+    }
+    public void deleteReview(String reviewId, String imdbId, String userId){
+        reviewRepository.deleteById(new ObjectId(reviewId));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().pull("reviewIds", Criteria.where("id").is(new ObjectId(reviewId))))
+                .first();
     }
 }
