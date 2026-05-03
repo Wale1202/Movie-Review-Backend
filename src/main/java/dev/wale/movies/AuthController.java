2cb831f52a8e7c20ba95e609ac734aa255a57a77
Wale1202 <83563997+Wale1202@users.noreply.github.com>
2026-05-03 12:09:49 +0100

    Updated Auth controller and Created the LoginRequest file

--- src/main/java/dev/wale/movies/AuthController.java
+++ src/main/java/dev/wale/movies/AuthController.java
@@ -5,6 +5,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.User;
+
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -15,6 +19,7 @@ public class AuthController {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody RegisterRequest request){
@@ -33,4 +38,14 @@ public class AuthController {
         String token = jwtService.generateToken(user);
         return ResponseEntity.ok(new AuthResponse(token));
     }
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@ResponseBody RegisterRequest request ){
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.email(), request.password()));
+
+        User user = userRepository.findByEmail(request.email()).orElseThrow();
+
+        String token = jwtService.generateToken(user);
+        return ResponseEntity.ok(new AuthResponse(token));
+    }
+
 }
---
+++ src/main/java/dev/wale/movies/LoginRequest.java
@@ -0,0 +1,8 @@
+package dev.wale.movies;
+
+public class LoginRequest {
+    public record LoginRequest(String email, String password) {
+        
+    }
+
+}
