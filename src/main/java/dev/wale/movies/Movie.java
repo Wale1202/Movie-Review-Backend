154723885663643650460ad8b928971707f29199
Wale1202 <83563997+Wale1202@users.noreply.github.com>
2026-05-10 17:40:43 +0100

    Updated the Movie file by adding addedBy

--- src/main/java/dev/wale/movies/Movie.java
+++ src/main/java/dev/wale/movies/Movie.java
@@ -24,6 +24,7 @@ public class Movie {
     private String releaseDate;
     private String trailerLink;
     private String poster;
+    private String addedBy;
     private List<String> genres;
     private List<String> backdrops;
     @DocumentReference
